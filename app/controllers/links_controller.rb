9db2ad988d4a47a115ef91c6037754cadd609924
Sofia Birto <sofiabitno@gmail.com>
2019-08-15 13:56:57 +0300

    correct data in share and notice not found

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -9,18 +9,11 @@ class LinksController < ApplicationController
   def show
     link = params[:source_link]
     @result = run(link)
-    verification
-    links_to_user(link)
+    binding.pry
+    links_to_user(link) unless @result == 'error'
     respond_to do |format|
       format.js
       format.json { render json: @result.to_json }
-    end
-  end
-
-  def verification
-    if @result == 'error'
-      flash[:notice] = 'Not found'
-      redirect_to root_path
     end
   end
 end
