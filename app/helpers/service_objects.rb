f1985b99fb891a1a5ff5f37678abe0e437960b60
Sofia Birto <sofiabitno@gmail.com>
2019-09-04 14:24:11 +0300

    add registration offer

--- app/helpers/service_objects.rb
+++ app/helpers/service_objects.rb
@@ -1,6 +1,7 @@
 module ServiceObjects
   def generate_links_info(link)
     @result = run(link)
+    flash[:notice] = "#{t('app.registration_offer')} - #{view_context.link_to(t('links.sign_up'), new_user_registration_path)}" unless current_user
   rescue RuntimeError
     flash[:error] = t('my_notice.not_found')
     respond_to do |format|
