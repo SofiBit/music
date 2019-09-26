bf7bf8fb56642e80dabb8cc9fad98649ddcfbfdc
Sofia Birto <sofiabitno@gmail.com>
2019-09-26 15:50:02 +0300

    add button clear

---
+++ app/assets/javascripts/clear_content.js
@@ -0,0 +1,6 @@
+function clearContent() {
+  let element = document.getElementById('show_div');
+  let form = document.getElementById('search-track');
+  form.value = "";
+  element.innerHTML = "";
+}
