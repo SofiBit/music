bb437e43562810ea89463e05262d9855cce82195
Sofia Birto <sofiabitno@gmail.com>
2019-11-13 20:32:24 +0300

    remove binding

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,6 @@ class User < ApplicationRecord
           :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-    binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.provider = auth.provider
       user.uid = auth.uid
